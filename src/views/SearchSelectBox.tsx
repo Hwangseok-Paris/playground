8d8a01bb4b37554e8cd147b1f35fc80a62c2da20
paris <hwangseok.paris@gmail.com>
2024-09-03 15:12:13 +0900

    240903: 검색가능한 셀렉트박스 작업 완료!

--- src/views/SearchSelectBox.tsx
+++ src/views/SearchSelectBox.tsx
@@ -1,18 +1,30 @@
-import React, { FC, useState } from "react";
+import React, { FC, useState, useEffect, useMemo } from "react";
 import { MagnifyingGlassIcon } from "@heroicons/react/24/solid";
 
 const SearchSelectBox: FC = () => {
-  const options: string[] = [
-    "올림픽공원",
-    "서울대공원",
-    "잠실종합운동장 주경기장",
-    "예스24라이브홀",
-  ];
+  const options: string[] = useMemo(() => {
+    return [
+      "올림픽공원",
+      "서울대공원",
+      "잠실종합운동장 주경기장",
+      "예스24라이브홀",
+      "장충체육관",
+      "클럽FF",
+      "송도 달빛축제공원",
+      "올림픽 체조경기장(KESPO DOM)",
+    ];
+  }, []);
 
-  const [inputValue, setInputValue] = useState<string>("");
-  const [filteredOptions, setFilteredOptions] = useState<string[]>([...options]);
-  const [inputFocus, setInputFocus] = useState(false);
+  const [inputValue, setInputValue] = useState<string>(""); // 텍스트박스 입력값
+  const [filteredOptions, setFilteredOptions] = useState<string[]>([...options]); // 필터링된 옵션 리스트
+  const [inputFocus, setInputFocus] = useState(false); // inputFocus 상태 > 옵션 표시여부 결정
+  const [checkValue, setCheckValue] = useState(false); // inputBox 내 값 유효성 체크용
 
+  /**
+   * handleInputChange
+   * 인풋박스 입력값 변경 감지 > 옵션 리스트 필터링
+   * @param e
+   */
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const value = e.target.value;
     setInputValue(value);
@@ -27,67 +39,108 @@ const SearchSelectBox: FC = () => {
     }
   };
 
+  /**
+   * handleOptionClick
+   * 옵션 항목 선택 핸들러
+   * @param option
+   * @returns
+   */
   const handleOptionClick = (option: string) => {
     setInputValue(option);
-    console.log(inputValue, option);
 
-    const filterd = options.filter((item) => {
-      item.toLowerCase().includes(option.toLowerCase());
-    });
+    if (option === "") {
+      return setFilteredOptions(options);
+    }
+    const filterd = options.filter((item) => item.toLowerCase().includes(option.toLowerCase()));
     setFilteredOptions(filterd);
-
-    console.log(filteredOptions);
-
     setInputFocus(false);
   };
 
+  /**
+   * handleBlur
+   * inputBox Div 아웃포커스시 옵션 리스트 닫기
+   * @param e
+   */
   const handleBlur = (e: React.FocusEvent<HTMLDivElement>) => {
-    // 포커스가 div 바깥으로 나갈 때만 드롭다운을 닫음
-    if (!e.currentTarget.contains(e.relatedTarget as Node)) {
+    if (!e.relatedTarget && !e.currentTarget.contains(e.relatedTarget)) {
       setInputFocus(false);
     }
   };
 
-  return (
-    <div className="relative w-full" onBlur={handleBlur} tabIndex={-1}>
-      <div className="relative flex">
-        <input
-          type="text"
-          value={inputValue}
-          onChange={handleInputChange}
-          className="border p-2 w-full"
-          placeholder="Type to filter options..."
-          onFocus={() => setInputFocus(true)}>
-          {/* ggg */}
-        </input>
-        <MagnifyingGlassIcon
-          className="flex h-full justify-center size-6 absolute right-2 opacity-50"
-          onClick={() => setInputFocus(true)}
-        />
-      </div>
-      {inputFocus && filteredOptions.length > 0 && (
-        <ul className="absolute border bg-white w-full mt-1 max-h-[200px] overflow-y-auto">
-          {filteredOptions.length > 0 ? (
-            filteredOptions.map((option, index) => (
-              <li
-                key={index}
-                className="p-2 hover:bg-gray-200 cursor-pointer"
-                onClick={() => handleOptionClick(option)}>
-                {option}
-              </li>
-            ))
-          ) : (
-            <li className="p-2 text-gray-500">No options found</li>
-          )}
-        </ul>
-      )}
-      {/* 
-      {!inputValue && (
-        <ul className="absolute border bg-white w-full mt-1 max-h-40 overflow-y-auto">
+  // inputValue 값 유효성 체크
+  useEffect(() => {
+    if (inputValue && options.includes(inputValue)) {
+      setCheckValue(true);
+    } else {
+      setCheckValue(false);
+    }
+  }, [inputValue, options]);
 
-        </ul>
-      )} */}
-    </div>
+  return (
+    <>
+      <div className="flex flex-col md:flex-row w-full">
+        <div
+          className="relative  max-w-[1000px] md:w-[300px] h-[75px]"
+          onBlur={handleBlur}
+          tabIndex={-1}>
+          <div className="relative flex">
+            <input
+              type="text"
+              value={inputValue}
+              onChange={handleInputChange}
+              className="border p-2 w-full"
+              placeholder="Type options..."
+              onFocus={() => setInputFocus(true)}
+            />
+            <MagnifyingGlassIcon
+              className="flex h-full justify-center size-6 absolute right-2 opacity-50"
+              onClick={() => setInputFocus(true)}
+            />
+          </div>
+          {inputFocus && (
+            <ul className="absolute border bg-white w-full mt-1 max-h-[200px] overflow-y-auto">
+              <li
+                key={0}
+                className="p-2 hover:bg-gray-200 cursor-pointer text-[#000000]/30"
+                onClick={() => handleOptionClick("")}>
+                select option
+              </li>
+              {filteredOptions.length > 0
+                ? filteredOptions.map((option, index) => (
+                    <li
+                      key={index + 1}
+                      className="p-2 hover:bg-gray-200 cursor-pointer"
+                      onClick={() => handleOptionClick(option)}>
+                      {option}
+                    </li>
+                  ))
+                : ""}
+            </ul>
+          )}
+        </div>
+        <div className="pt-[200px] md:pl-10 md:pt-0 md:w-[500px]">
+          <div className="flex gap-1">
+            FOCUS SATATUS :
+            {inputFocus ? (
+              <div className="text-[green]">TURE</div>
+            ) : (
+              <div className="text-[red]">FALSE</div>
+            )}
+          </div>
+          <div>
+            FILTERD OPTION LIST : {filteredOptions.length == 0 ? "EMPTY" : filteredOptions.length}
+          </div>
+          <div className="flex gap-1">
+            VALIDATION CHECK :
+            {checkValue ? (
+              <div className="text-[green]">SUCCESS</div>
+            ) : (
+              <div className="text-[red]">FAIL</div>
+            )}
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 export default SearchSelectBox;
